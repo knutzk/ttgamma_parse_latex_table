aded4551a6986bde8c87abd46beb7b6a4ac2ef31
Knut Zoch <knut.zoch@gmail.com>
2017-11-01 15:28:02 +0100

    Write table headers to latex file

--- table_to_file.py
+++ table_to_file.py
@@ -9,6 +9,14 @@ def storeJSON(table, file_string):
 
 def storeTEX(table, file_string):
     with open(file_string, 'w') as fp:
-
         fp.write("\\begin{tabular}\n")
+        fp.write("  \\hline\n")
+        fp.write("  ")
+        # First we need to write out the headers
+        for row in table.getEntries():
+            for column in table.getEntries()[row]:
+                fp.write("& %s " % (column))
+            fp.write("\\\\\n")
+            break
+        fp.write("  \\hline\n")
         fp.write("\\end{tabular}\n")

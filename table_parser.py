75705c0cbd4a6b501e5520adcef7df2c16638515
Knut Zoch <knut.zoch@gmail.com>
2017-11-01 14:44:36 +0100

    Added if "__main__" to catch main executor

--- table_parser.py
+++ table_parser.py
@@ -2,12 +2,13 @@
 
 import latex_table
 
-table = latex_table.readFromLatex("table.tex")
+if __name__ == "__main__":
+    table = latex_table.readFromLatex("table.tex")
 
-rows = table.getRows()
-columns = table.getColumns()
-dict = table.getEntries()
+    rows = table.getRows()
+    columns = table.getColumns()
+    dict = table.getEntries()
 
-for row in rows:
-    for column in columns:
-        print "%s %s %s" % (row, column, dict[row][column])
+    for row in rows:
+        for column in columns:
+            print "%s %s %s" % (row, column, dict[row][column])

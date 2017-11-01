d5eee30f1cdc8580411b18b88a1d9d39c3dca3c0
Knut Zoch <knut.zoch@gmail.com>
2017-11-01 16:57:09 +0100

    Also save the not-inversed dictionary in SystDictionary

--- syst_calculator.py
+++ syst_calculator.py
@@ -8,19 +8,26 @@ import json
 class SystDictionary():
     _groups = []       #List of all groups
     _systematics = []  #List of all systematics
-    _inv_dict = {}     #Reverse dictionary to look up groups
+    _dict = {}         #Normal dictionary to look up systematics of a group
+    _inv_dict = {}     #Reverse dictionary to look up group of a systematic
 
     def __init__(self, file_string):
         with open(file_string, 'r') as data_file:
-            data = json.load(data_file)
-        for g in data:
+            self._dict = json.load(data_file)
+        for g in self._dict:
             self._groups.append((g))
-            for s in data[g]:
+            for s in self._dict[g]:
                 self._inv_dict[s] = g
                 self._systematics.append(s)
 
-    def getGroup(self, syst):
+    # Look up a systematic in the inverse dictionary.
+    def lookupSystematic(self, syst):
         return self._inv_dict[syst]
 
+    # Look up a group in the dictionary.
+    def lookupGroup(self, group):
+        return self._dict[group]
+
+    # Return a list of all systematics.
     def getSystematics(self):
         return self._systematics
